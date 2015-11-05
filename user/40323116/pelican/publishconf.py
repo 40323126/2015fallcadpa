5b890d17c0205983add90cc22b3c04765ca069c6
40323116 <pkluke2300@gmail.com>
2015-11-05 16:02:43 +0800

    40323116

--- user/40323116/pelican/publishconf.py
+++ user/40323116/pelican/publishconf.py
@@ -26,7 +26,7 @@ AUTHOR_FEED_RSS = None
 
 # Blogroll
 LINKS = (('2015課程網頁', 'http://wordpress-2015course.rhcloud.com/'),
-         ('Python', 'http://python.org/'),('2015作業主頁', '../../') ('個人作業', 'http://pkluke22.github.io/2015cadp1/'))
+         ('Python', 'http://python.org/'),('2015作業主頁', '../../'),('個人作業', 'http://pkluke22.github.io/2015cadp1/'))
 
 # Social widget
 SOCIAL = (('twitter', 'http://twitter.com/ametaireau'),
