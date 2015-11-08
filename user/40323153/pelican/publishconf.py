f18cefb7df46b0d8c883522353935d63c5559ba2
peipei9728 <40323153@gm.nfu.edu.tw>
2015-11-08 16:16:12 +0800

    40323153

--- user/40323153/pelican/publishconf.py
+++ user/40323153/pelican/publishconf.py
@@ -26,7 +26,7 @@ AUTHOR_FEED_RSS = None
 
 # Blogroll
 LINKS = (('2015課程網頁', 'http://wordpress-2015course.rhcloud.com/'),
-         ('Python', 'http://python.org/'),('2015作業主頁', '../../'),(' my github', 'https://github.com/40323153/cadpa'),(' my HW pages', 'http://40323153.github.io/cadpa/'),(' my Vimeo', 'https://vimeo.com/home/myvideos'))
+         ('Python', 'http://python.org/'),('2015作業主頁', '../../'),(' my github', 'https://github.com/peipei9728'),(' my HW pages', 'http://peipei9728.github.io/cadpaw2/'),(' my Vimeo', 'https://vimeo.com/home/myvideos'))
 
 # Social widget
 SOCIAL = (('twitter', 'http://twitter.com/ametaireau'),
