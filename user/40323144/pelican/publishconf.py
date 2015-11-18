1967ce79ee37bd39669c2875f3f2d7105b17dd83
40323136 <s56428073@gmail.com>
2015-11-18 21:10:34 +0800

    40323109

--- user/40323144/pelican/publishconf.py
+++ user/40323144/pelican/publishconf.py
@@ -26,7 +26,7 @@ AUTHOR_FEED_RSS = None
 
 # Blogroll
 LINKS = (('2015課程網頁', 'http://wordpress-2015course.rhcloud.com/'),
-         ('Python', 'http://python.org/'),('2015作業主頁', '../../'))
+         ('Python', 'http://python.org/'),('2015作業主頁', '../../'),('個人作業', 'http://40323144.github.io/test/'))
 
 # Social widget
 SOCIAL = (('twitter', 'http://twitter.com/ametaireau'),
