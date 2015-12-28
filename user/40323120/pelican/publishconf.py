b42eea214f8b6f340a6a6d756df0cb931480fbb3
40323123 <smpss91341@gmail.com>
2015-12-28 20:37:02 +0800

    40323120

--- user/40323120/pelican/publishconf.py
+++ user/40323120/pelican/publishconf.py
@@ -25,12 +25,12 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('2015課程網頁', 'http://wordpress-2015course.rhcloud.com/'),
+LINKS = (('2015', 'http://wordpress-2015course.rhcloud.com/'),
          ('Python', 'http://python.org/'),('2015作業主頁', '../../'))
 
 # Social widget
-SOCIAL = (('twitter', 'http://twitter.com/ametaireau'),
-          ('github', 'http://github.com/ametaireau'),)
+SOCIAL = (
+          ('我的github', 'http://github.com/ametaireau'),)
           
 DEFAULT_PAGINATION = 10
 
