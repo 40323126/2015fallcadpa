8cf9697d0105e7c80f643a6eb6130ec86864dad5
40323148 <40323148@gm.nfu.edu.tw>
2016-01-10 16:05:41 +0800

    1008

--- user/40323148/pelican/publishconf.py
+++ user/40323148/pelican/publishconf.py
@@ -26,7 +26,7 @@ AUTHOR_FEED_RSS = None
 
 # Blogroll
 LINKS = (('2015課程網頁', 'http://wordpress-2015course.rhcloud.com/'),
-         ('Python', 'http://python.org/'),(' my github', 'https://github.com/40323148/cadpa'),(' my youtube', 'https://www.youtube.com/channel/UCGFyI7tfNCDldL3KxBRDaoA'),(' my class ', 'http://2015fallhw.github.io/2015fallcadpa/user/40323148/'),(' my group', 'http://2015fallhw.github.io/2015fallcadpa/category/g2.html'))
+         ('Python', 'http://python.org/'),(' my github', 'https://github.com/40323148/cadpa'),(' my youtube', 'https://www.youtube.com/channel/UCGFyI7tfNCDldL3KxBRDaoA'),(' my page ', 'http://40323148.github.io/cadpa/'),(' my group', 'http://2015fallhw.github.io/2015fallcadpa/category/g2.html'))
 
 # Social widget
 SOCIAL = (('twitter', 'http://twitter.com/ametaireau'),
