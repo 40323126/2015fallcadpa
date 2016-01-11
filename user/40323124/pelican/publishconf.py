5447fa27af863b8db37f6deb36444a6795bcf277
40323124 <40323124@gm.nfu.edu.tw>
2016-01-11 18:31:23 +0800

    g1

--- user/40323124/pelican/publishconf.py
+++ user/40323124/pelican/publishconf.py
@@ -26,7 +26,7 @@ AUTHOR_FEED_RSS = None
 
 # Blogroll
 LINKS = (('2015課程網頁', 'http://wordpress-2015course.rhcloud.com/'),
-         ('Python', 'http://python.org/'),('2015作業主頁', '../../'),('my github', 'https://github.com/40323124/2015cadp1/tree/gh-pages'),('個人網頁','http://40323124.github.io/2015cadp1/'))
+         ('Python', 'http://python.org/'),('2015作業主頁', '../../'),('my github', 'https://github.com/40323124/2015cadp1/tree/gh-pages'),('個人網頁','http://40323124.github.io/2015cadp1/'),('g1', 'http://2015fallhw.github.io/2015fallcadpa/category/g1.html'))
 
 # Social widget
 SOCIAL = (('twitter', 'http://twitter.com/ametaireau'),
